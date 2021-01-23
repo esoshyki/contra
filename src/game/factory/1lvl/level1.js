5dd3fd6a324e8a3127c612157b547567d6294647
bexon26 <47786103+bexon26@users.noreply.github.com>
2021-01-23 13:11:51 +0300

    working  1lvl

--- src/game/factory/1lvl/level1.js
+++ src/game/factory/1lvl/level1.js
@@ -13,7 +13,7 @@ const floor = height - defaultSize * 5;
 
 
 const backgrounds = [
-	Farm.hills.element(0, height - defaultSize * 3 - Farm.hills.height, levelWidth )
+	Farm.hills.element(0, height - defaultSize * 3 - Farm.hills.height, levelWidth)
 	// Nature.backgrounds.trees.big(250, floor, 20),
 	// Nature.backgrounds.bush.big.yellow(800, floor, 30),
 	// Farm.houses.square(defaultSize * 10, height - defaultSize * 5, 200)
@@ -21,7 +21,7 @@ const backgrounds = [
 
 const loadBackgrounds = factory => {
 	backgrounds.forEach((el, idx) => {
-		const entity = new BgItem({...el, factory, element: "water"});
+		const entity = new BgItem({ ...el, factory, element: "water" });
 		factory.addToBodies(entity.body);
 		factory.addToEntities(entity);
 	});
@@ -29,141 +29,55 @@ const loadBackgrounds = factory => {
 
 const ground = [
 
-	Nature.statics.ground.upper.left(defaultSize * 0, height - defaultSize * 6),
-	Nature.statics.ground.corner.left.vertical(0 , height - defaultSize * 5, defaultSize * 5),
-	Nature.statics.ground.upper.middle(defaultSize * 1, height - defaultSize * 6, defaultSize * 3),
-	Nature.statics.ground.upper.right(defaultSize * 4, height - defaultSize * 6),
-	Nature.statics.ground.corner.right.vertical(defaultSize * 4 , height - defaultSize * 5, defaultSize * 5),
+	Nature.statics.ground.upper.left(defaultSize * 0, height - defaultSize * 2),
+	Nature.statics.ground.corner.left.vertical(0, height - defaultSize, defaultSize * 3),
+	Nature.statics.ground.upper.middle(defaultSize * 1, height - defaultSize * 2, defaultSize * 2),
+	// Nature.statics.ground.upper.right(defaultSize * 4, height - defaultSize * 6),
+	Nature.statics.ground.corner.right.vertical(defaultSize * 5, height - defaultSize * 5, defaultSize * 7),
 
-	Nature.statics.ground.inside.middle(defaultSize, height - defaultSize * 5, defaultSize * 3, defaultSize * 6),
+	Nature.statics.ground.inside.middle(defaultSize, height - defaultSize, defaultSize * 4, defaultSize * 3),
+
+	Nature.statics.ground.ledge.left(defaultSize * 0, height + defaultSize * 2),
+	Nature.statics.ground.ledge.middle(defaultSize * 1, height + defaultSize * 2, defaultSize * 4),
+
+
 
 	Nature.statics.ground.upper.left(defaultSize * 10, height - defaultSize * 6),
-	Nature.statics.ground.corner.left.vertical( defaultSize * 10, height - defaultSize * 5, defaultSize * 5),
+	Nature.statics.ground.corner.left.vertical(defaultSize * 10, height - defaultSize * 5, defaultSize * 5),
 	Nature.statics.ground.upper.middle(defaultSize * 11, height - defaultSize * 6, defaultSize * 3),
 	Nature.statics.ground.upper.right(defaultSize * 14, height - defaultSize * 6),
-	Nature.statics.ground.corner.right.vertical(defaultSize * 14 , height - defaultSize * 5, defaultSize * 5),
+	Nature.statics.ground.corner.right.vertical(defaultSize * 14, height - defaultSize * 5, defaultSize * 5),
 
 	Nature.statics.ground.inside.middle(defaultSize * 11, height - defaultSize * 5, defaultSize * 3, defaultSize * 6),
-	// // Nature.statics.ground.inside.left(defaultSize * 9, height - defaultSize * 3),
-	// // Nature.statics.ground.inside.middle(defaultSize * 10, height - defaultSize * 3),
-	// // Nature.statics.ground.inside.middle(defaultSize * 11, height - defaultSize * 3),
-	// // Nature.statics.ground.inside.middle(defaultSize * 12, height - defaultSize * 3),
-	// // Nature.statics.ground.inside.right(defaultSize * 13, height - defaultSize * 3),
-
-	// // Nature.statics.ground.inside.left(defaultSize * 9, height - defaultSize * 2),
-	// // Nature.statics.ground.inside.middle(defaultSize * 10, height - defaultSize * 2),
-	// // Nature.statics.ground.inside.middle(defaultSize * 11, height - defaultSize * 2),
-	// // Nature.statics.ground.inside.middle(defaultSize * 12, height - defaultSize * 2),
-	// // Nature.statics.ground.inside.right(defaultSize * 13, height - defaultSize * 2),
-
-	// // Nature.statics.ground.inside.left(defaultSize * 9, height - defaultSize),
-	// // Nature.statics.ground.inside.middle(defaultSize * 10, height - defaultSize),
-	// // Nature.statics.ground.inside.middle(defaultSize * 11, height - defaultSize),
-	// // Nature.statics.ground.inside.middle(defaultSize * 12, height - defaultSize),
-	// // Nature.statics.ground.inside.right(defaultSize * 13, height - defaultSize),
-
-	// Nature.statics.ground.ledge.left(defaultSize * 9, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 10, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 11, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 12, height),
-	// Nature.statics.ground.ledge.right(defaultSize * 13, height),
 
 
 
-	// // Nature.statics.water.inside(defaultSize * 14, height),
-	// // Nature.statics.water.inside(defaultSize * 15, height),
-	// // Nature.statics.water.inside(defaultSize * 16, height),
-
-	// Nature.statics.ground.ledge.middle(defaultSize * 17, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 18, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 19, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 20, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 21, height),
-	// Nature.statics.ground.ledge.middle(defaultSize * 22, height),
-
-	// Nature.statics.ground.upper.left(0, height - defaultSize * 4),
-	// Nature.statics.ground.upper.middle(defaultSize, height - defaultSize * 4),
-	// Nature.statics.ground.upper.middle(defaultSize * 2, height - defaultSize * 4),
-	// Nature.statics.ground.corner.left.horisontal(defaultSize * 3, height - defaultSize * 4),
-	// Nature.statics.ground.corner.left.corner(defaultSize * 4, height - defaultSize * 4),
-	// Nature.statics.ground.corner.left.vertical(defaultSize * 4, height - defaultSize * 5),
-	// Nature.statics.ground.inside.left(defaultSize * 4, height - defaultSize * 6),
-	// Nature.statics.ground.upper.left(defaultSize * 4, height - defaultSize * 7),
-	// Nature.statics.ground.upper.right(defaultSize * 5, height - defaultSize * 7),
-	// Nature.statics.ground.inside.right(defaultSize * 5, height - defaultSize * 6),
-	// Nature.statics.ground.inside.right(defaultSize * 5, height - defaultSize * 5),
-	// Nature.statics.ground.inside.right(defaultSize * 5, height - defaultSize * 4),
-
-
-
-
-
-
-
-
-
-	// Nature.statics.ground.cornice.left(defaultSize * 18, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.middle(defaultSize * 19, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.middle(defaultSize * 20, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.middle(defaultSize * 21, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.right(defaultSize * 22, height - defaultSize * 6),
-
-	// Nature.statics.ground.cornice.left(defaultSize * 26, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.middle(defaultSize * 27, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.middle(defaultSize * 28, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.right(defaultSize * 29, height - defaultSize * 6),
-
-	// Nature.statics.ground.cornice.left(defaultSize * 34, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.middle(defaultSize * 35, height - defaultSize * 6),
-	// Nature.statics.ground.cornice.right(defaultSize * 36, height - defaultSize * 6),
 ];
 
 const loadGround = factory => {
 	ground.forEach((el) => {
 		console.log('el')
 		console.log(el)
-		const entity = new StaticItem({...el, factory, element: "ground"});
+		const entity = new StaticItem({ ...el, factory, element: "ground" });
 		factory.addToBodies(entity.body);
-		factory.addToEntities(entity);	
+		factory.addToEntities(entity);
 	})
 };
 
 console.log('fdadfdfdafhadgiuad hguoida hug')
-console.log(Nature.statics.water.inside(defaultSize * 0, height - defaultSize * 3, defaultSize * 25, defaultSize * 6 ))
+console.log(Nature.statics.water.inside(defaultSize * 0, height - defaultSize * 3, defaultSize * 25, defaultSize * 6))
 
 const water = [
 
 	Nature.statics.water.up(defaultSize * 0, height - defaultSize * 3 + 17, defaultSize * 25),
-	Nature.statics.water.inside(defaultSize * 0, height - defaultSize * 3, defaultSize * 25, defaultSize * 6 ),
+	Nature.statics.water.inside(defaultSize * 0, height - defaultSize * 3, defaultSize * 25, defaultSize * 6),
 
-	// Nature.statics.water.inside(defaultSize * 5, height - defaultSize * 3, ),
-	// Nature.statics.water.inside(defaultSize * 6, height - defaultSize * 3),
-	// Nature.statics.water.inside(defaultSize * 7, height - defaultSize * 3),
-	// Nature.statics.water.inside(defaultSize * 8, height - defaultSize * 3),
-	// Nature.statics.water.inside(defaultSize * 9, height - defaultSize * 3),
 
-	// Nature.statics.water.inside(defaultSize * 5, height - defaultSize * 2),
-	// Nature.statics.water.inside(defaultSize * 6, height - defaultSize * 2),
-	// Nature.statics.water.inside(defaultSize * 7, height - defaultSize * 2),
-	// Nature.statics.water.inside(defaultSize * 8, height - defaultSize * 2),
-	// Nature.statics.water.inside(defaultSize * 9, height - defaultSize * 2),
-
-	// Nature.statics.water.inside(defaultSize * 5, height - defaultSize),
-	// Nature.statics.water.inside(defaultSize * 6, height - defaultSize),
-	// Nature.statics.water.inside(defaultSize * 7, height - defaultSize),
-	// Nature.statics.water.inside(defaultSize * 8, height - defaultSize),
-	// Nature.statics.water.inside(defaultSize * 9, height - defaultSize),
-
-	// Nature.statics.water.inside(defaultSize * 5, height),
-	// Nature.statics.water.inside(defaultSize * 6, height),
-	// Nature.statics.water.inside(defaultSize * 7, height),
-	// Nature.statics.water.inside(defaultSize * 8, height),
-	// Nature.statics.water.inside(defaultSize * 9, height),
 ];
 
 const loadWater = factory => {
 	water.forEach((el, idx) => {
-		const entity = new WaterItem({...el, factory, element: "water"});
+		const entity = new WaterItem({ ...el, factory, element: "water" });
 		factory.addToBodies(entity.body);
 		factory.addToEntities(entity);
 	})
@@ -171,8 +85,8 @@ const loadWater = factory => {
 
 const loadtTriggers = (factory) => {
 	factory.triggers = [
-		{ 
-			condition: factory => factory.entities.player.body.position.x >= 200 && factory.triggers[0].done === false, 
+		{
+			condition: factory => factory.entities.player.body.position.x >= 200 && factory.triggers[0].done === false,
 			action: (factory) => factory.addBird.call(factory, 1500, 200),
 			done: false,
 		},
@@ -185,7 +99,7 @@ const loadtTriggers = (factory) => {
 };
 
 const loadPlayer = (factory) => {
-	const player = new Player({left: 100, top: 100, factory });
+	const player = new Player({ left: 100, top: 100, factory });
 	factory.entities.player = player;
 	factory.addToBodies(player.body);
 }
--- src/game/factory/1lvl/nature/nature.js
+++ src/game/factory/1lvl/nature/nature.js
@@ -5,6 +5,7 @@ import waterUpPng from './waterUp.png';
 import groundUpPng from './assets/groundUp.png';
 import groundYleftPng from './assets/groundVerticalLeft.png';
 import groundYrightPng from './assets/groundVerticalRight.png';
+import groundLedgeInsidePng from './assets/groundLedgeInside.png';
 import groundUpCorner from './assets/groundUpCorner.png';
 
 const asset = `url(${png})`
@@ -19,71 +20,71 @@ const mirror = "180deg";
 const statics = {
 
   groundUpper: {
-    right : { width: 74, height: 74, bgx: -270, bgy: -18},
-    middle : { width: 74, height: 74, bgx: -185, bgy: -18},
-    left : { width: 74, height: 74, bgx: -96, bgy: -18},
+    right: { width: 74, height: 74, bgx: -270, bgy: -18 },
+    middle: { width: 74, height: 74, bgx: -185, bgy: -18 },
+    left: { width: 74, height: 74, bgx: -96, bgy: -18 },
   },
 
   groundRotateRight: {
-    x: { width: 74, height: 74, bgx: -354, bgy: -189},
-    xy : { width: 74, height: 74, bgx: -269, bgy: -189},
-    y : { width: 74, height: 74, bgx: -269, bgy: -104},
+    x: { width: 74, height: 74, bgx: -354, bgy: -189 },
+    xy: { width: 74, height: 74, bgx: -269, bgy: -189 },
+    y: { width: 74, height: 74, bgx: -269, bgy: -104 },
   },
 
   groundRotateLeft: {
-    x : { width: 74, height: 74, bgx: -14, bgy: -189},
-    xy : { width: 74, height: 74, bgx: -98, bgy: -189}, 
-    y : { width: 74, height: 74, bgx: -98, bgy: -104},
+    x: { width: 74, height: 74, bgx: -14, bgy: -189 },
+    xy: { width: 74, height: 74, bgx: -98, bgy: -189 },
+    y: { width: 74, height: 74, bgx: -98, bgy: -104 },
   },
 
-  /* Выступы */   
+  /* Выступы */
   groundLedge: {
-    right : { width: 74, height: 74, bgx: -354, bgy: -272},
-    middle : { width: 74, height: 74, bgx: -185, bgy: -189},
-    left : { width: 74, height: 74, bgx: -14, bgy: -272},
+    right: { width: 74, height: 74, bgx: -354, bgy: -272 },
+    middle: { width: 74, height: 74, bgx: -185, bgy: -189 },
+    left: { width: 74, height: 74, bgx: -14, bgy: -272 },
   },
 
   /* Карнизы */
   groundCornice: {
-    right : { width: 74, height: 57, bgx: -269, bgy: -272},
-    middle : { width: 74, height: 57, bgx: -185, bgy: -272},
-    left : { width: 74, height: 57, bgx: -98, bgy: -272},
+    right: { width: 74, height: 57, bgx: -269, bgy: -272 },
+    middle: { width: 74, height: 57, bgx: -185, bgy: -272 },
+    left: { width: 74, height: 57, bgx: -98, bgy: -272 },
   },
 
   /* Внутри */
-  groundInside : {
-    left: { width: 74, height: 74, bgx: -98, bgy: -104}, 
-    middle: { width: 74, height: 74, bgx: -185, bgy: -104},
-    right: { width: 74, height: 74, bgx: -269, bgy: -104}
+  groundInside: {
+    left: { width: 74, height: 74, bgx: -98, bgy: -104 },
+    middle: { width: 74, height: 74, bgx: -185, bgy: -104 },
+    right: { width: 74, height: 74, bgx: -269, bgy: -104 }
   },
-  
+
   /* Вода */
   water: {
-    inside: { width: 74, height: 74, bgx: -353, bgy: -104},
-    up: { width: 74, height: 74, bgx: -353, bgy: -20}
+    inside: { width: 74, height: 74, bgx: -353, bgy: -104 },
+    up: { width: 74, height: 74, bgx: -353, bgy: -20 }
   },
-  
-  box : { width: 44, height: 45, bgx: -455, bgy: -33},
+
+  box: { width: 44, height: 45, bgx: -455, bgy: -33 },
 };
 
 const bgs = {
-  plate : { width: 36, height: 38, bgx: -514, bgy: -39 },
-  pointer : { width: 36, height: 38, bgx: -567, bgy: -39 },
-  mashroomOrange : { width: 29, height: 23, bgx: -649, bgy: -53 },
-  mashroomPink : { width: 29, height: 23, bgx: -616, bgy: -53 },
-  stump : { width: 76, height: 25, bgx: -696, bgy: -68 },
+  plate: { width: 36, height: 38, bgx: -514, bgy: -39 },
+  pointer: { width: 36, height: 38, bgx: -567, bgy: -39 },
+  mashroomOrange: { width: 29, height: 23, bgx: -649, bgy: -53 },
+  mashroomPink: { width: 29, height: 23, bgx: -616, bgy: -53 },
+  stump: { width: 76, height: 25, bgx: -696, bgy: -68 },
   trees: {
     big: { width: 167, height: 182, bgx: -448, bgy: -91 },
     small: { width: 167, height: 165, bgx: -620, bgy: -107 }
   },
   bush: {
     big: {
-      green: { width: 82, height: 38, bgx: -479, bgy: -301},
-      yellow: { width: 82, height: 38, bgx: -566, bgy: -301}
+      green: { width: 82, height: 38, bgx: -479, bgy: -301 },
+      yellow: { width: 82, height: 38, bgx: -566, bgy: -301 }
     },
     small: {
-      green: { width: 42, height: 38, bgx: -672, bgy: -307},
-      yellow: { width: 42, height: 38, bgx: -726, bgy: -301}
+      green: { width: 42, height: 38, bgx: -672, bgy: -307 },
+      yellow: { width: 42, height: 38, bgx: -726, bgy: -301 }
     }
   }
 };
@@ -106,44 +107,48 @@ export default {
 
     ground: {
       upper: {
-        left: (left, top) => ({left, top, width: 74, height: 74, asset: getAsset(groundUpCorner), rotateY: mirror,}),
+        left: (left, top) => ({ left, top, width: 74, height: 74, asset: getAsset(groundUpCorner), rotateY: mirror, }),
         middle: (left, top, width) => ({
           left, top, width, height: 74, asset: getAsset(groundUpPng), bgy: 1
         }),
-        right: (left, top) => ({left, top, width: 74, height: 74, asset: getAsset(groundUpCorner), }),        
+        right: (left, top) => ({ left, top, width: 74, height: 74, asset: getAsset(groundUpCorner), }),
       },
       inside: {
         left: (left, top) => getItem(left, top, statics.groundInside.left),
-        middle: (left, top, w, h) => ({left, top, height: h, width: w, asset: `url(${groundPng})`}),
+        middle: (left, top, w, h) => ({ left, top, height: h, width: w, asset: `url(${groundPng})` }),
         right: (left, top) => getItem(left, top, statics.groundInside.right),
       },
       corner: {
         left: {
           horisontal: (left, top) => getItem(left, top, statics.groundRotateLeft.x),
           corner: (left, top) => getItem(left, top, statics.groundRotateLeft.xy),
-          vertical: (left, top, height) => ({left, top, height, width: 74, asset: getAsset(groundYleftPng)}),
+          vertical: (left, top, height) => ({ left, top, height, width: 74, asset: getAsset(groundYleftPng) }),
         },
         right: {
           horisontal: (left, top) => getItem(left, top, statics.groundRotateRight.x),
           corner: (left, top) => getItem(left, top, statics.groundRotateRight.xy),
-          vertical: (left, top, height) => ({left, top, height, width: 74, asset: getAsset(groundYleftPng), rotateY: mirror}),
-        },       
+          vertical: (left, top, height) => ({ left, top, height, width: 74, asset: getAsset(groundYleftPng), rotateY: mirror }),
+        },
       },
       ledge: {
         left: (left, top) => getItem(left, top, statics.groundLedge.left),
-        middle: (left, top) => getItem(left, top, statics.groundLedge.middle),
+        middle: (left, top, width) => ({
+          left, top, width, height: 74, asset: getAsset(groundLedgeInsidePng)
+        }),
+
+        //getItem(left, top, statics.groundLedge.middle),
         right: (left, top) => getItem(left, top, statics.groundLedge.right),
       },
       cornice: {
         left: (left, top) => getItem(left, top, statics.groundCornice.left),
         middle: (left, top) => getItem(left, top, statics.groundCornice.middle),
-        right: (left, top) => getItem(left, top, statics.groundCornice.right),       
+        right: (left, top) => getItem(left, top, statics.groundCornice.right),
       }
     },
 
     water: {
-      up: (left, top, w) => ({left, top: top - 57, width: w, height: 57, asset: `url(${waterUpPng})`}),
-      inside: (left, top, width, height) => ({left, top: top, width, height, asset: `url(${waterPng})`}),
+      up: (left, top, w) => ({ left, top: top - 57, width: w, height: 57, asset: `url(${waterUpPng})` }),
+      inside: (left, top, width, height) => ({ left, top: top, width, height, asset: `url(${waterPng})` }),
     }
 
   },
@@ -163,13 +168,13 @@ export default {
     bush: {
       big: {
         green: (left, top, perspective) => getItem(left, top, bgs.bush.big.green, perspective),
-        yellow: (left, top, perspective) => getItem(left, top, bgs.bush.big.yellow, perspective),      
+        yellow: (left, top, perspective) => getItem(left, top, bgs.bush.big.yellow, perspective),
       },
       small: {
         green: (left, top, perspective) => getItem(left, top, bgs.bush.small.green, perspective),
-        yellow: (left, top, perspective) => getItem(left, top, bgs.bush.small.yellow, perspective),      
+        yellow: (left, top, perspective) => getItem(left, top, bgs.bush.small.yellow, perspective),
       }
     }
-    
+
   }
 }
