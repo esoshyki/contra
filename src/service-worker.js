cbd2670f688636dcbfdc3eb4ce8da4c43acb9142
esoshyki <esoshyki@yandex.by>
2021-01-30 18:54:20 +0300

    delete build

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,5 @@
+import {precacheAndRoute} from 'workbox-precaching';
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -33,7 +35,6 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
